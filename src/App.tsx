48bcfe560999745250f436dd2b18effd4a311661
Ali Soltani <alisolinejad@gmail.com>
2026-01-03 00:02:22 +0330

    feat: add circular progress component with centered value display

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import CircularProgress from "./components/CircularProgress";
 function App() {
   return (
     <>
-      <h1 className="text-8xl pl-4 text-amber-600  drop-shadow-[0_0_4px_#ee6612]">
+      <h1 className="text-7xl pl-4 text-amber-600 tracking-widest  drop-shadow-[0_0_4px_#ee6612]">
         circular-clock
       </h1>
-      <hr
-        className="divider-neon "
-      />
-      <h5 className=" text-2xl pl-4 text-emerald-400 ">
+      <hr className="divider-neon " />
+      <h5 className=" text-lg pl-4 text-emerald-400 tracking-widest">
         A stunning real-time neon clock with animated spinning border. Built
-        with <span className="text-red-400  glass text-lg">React</span> , 
-        <span className="text-red-400 glass text-lg">TypeScript </span> & 
-        <span className="text-red-400 glass text-lg">Tailwind CSS</span>  ; 
+        with <span className="text-red-400  glass text-lg">React</span> ,
+        <span className="text-red-400 glass text-lg">TypeScript </span> &
+        <span className="text-red-400 glass text-lg">Tailwind CSS</span> ;
       </h5>
-      <hr className="divider-neon"/>
+      <hr className="divider-neon" />
+
+     <div className="h-72 flex flex-row flex-wrap justify-center items-center gap-8 md:gap-16">
+      <CircularProgress value={16} max={24} label="Hours" color="#8b5cf6" />:
+      <CircularProgress value={45} max={60} label="Minutes" color="#3b82f6" />:
+       <CircularProgress value={20} max={60} label="Seconds" color="#10b981" />
+     </div>
     </>
   );
 }
--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -1,6 +1,16 @@
 import React from "react";
-
-const CircularProgress: React.FC = () => {
+interface CircularProgressProps {
+  value: number;
+  max: number;
+  label: string;
+  color: string;
+}
+const CircularProgress: React.FC<CircularProgressProps> = ({
+  value,
+  max,
+  label,
+  color,
+}) => {
   // --- محاسبات مهندسی و هندسی SVG ---
   const size = 150;
   const strokeWidth = 10;
@@ -11,25 +21,65 @@ const CircularProgress: React.FC = () => {
    * به دلیل ضخامت خط، از کادر SVG بیرون نزند (Pixel Perfect).
    */
   const radius = (size - strokeWidth) / 2;
+  /**
+   * محاسبه محیط دایره (Circumference):
+   * این عدد به عنوان طول کل "نوار پیشرفت" در ویژگی stroke-dasharray استفاده می‌شود.
+   */
+  const circumference = 2 * Math.PI * radius;
 
+  /**
+   * محاسبه آفست (Offset) - جادوی حرکت دایره:
+   * این مقدار تعیین می‌کند که چه مقدار از محیط دایره "خالی" بماند.
+   * فرمول: (محیط کل) منهای (درصد طی شده از محیط).
+   * value=0 => آفست برابر با کل محیط است (دایره خالی دیده می‌شود).
+   * value=max => آفست صفر است (دایره کامل دیده می‌شود).
+   */
+  const offset = circumference - (value / max) * circumference;
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="relative" style={{ width: size, height: size }}>
-
-        {/*  با استفاده از -rotate-90، نقطه شروع رسم را از سمت راست به بالای دایره منتقل می‌کنیم.  */}
-        <svg width={size} height={size} className="transform -rotate-90">
-
+        <svg width={size} height={size} >
           {/*  این دایره لایه‌ی زیرین است که مسیر حرکت تایمر را نشان می‌دهد.  */}
           <circle
             cx={size / 2} // مختصات مرکز دایره در محور X (دقیقاً وسط باکس)
             cy={size / 2} // مختصات مرکز دایره در محور Y (دقیقاً وسط باکس)
             r={radius} //شعاع دایره
-            stroke="#1e293b" 
-            strokeWidth={strokeWidth} 
+            stroke="#1e293b"
+            strokeWidth={strokeWidth}
             fill="transparent"
           />
 
+          <circle
+           /*  با استفاده از -rotate-90، نقطه شروع رسم را از سمت راست به بالای دایره منتقل می‌کنیم.  */
+           className="transform -rotate-90 origin-center"
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            stroke={color}
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            /* ۱. ایجاد خط‌چین: طول هر تکه خط برابر با کل محیط دایره است */
+            strokeDasharray={circumference}
+            /* ۲. جابجایی خط‌چین (آفست): تعیین می‌کند چقدر از خط دیده شود */
+            strokeDashoffset={offset}
+            /* ۳. گرد کردن لبه‌های خط (برای زیبایی بیشتر) */
+            strokeLinecap="round"
+            style={{
+           /* ۴. انیمیشن نرم: وقتی offset تغییر می‌کند، ۱ ثانیه طول بکشد تا به جای جدید برسد */   
+              transition: "stroke-dashoffset 1s linear",
+              /* ۵. درخشش نئونی: ایجاد یک سایه رنگی دور خط */
+              filter: `drop-shadow(0 0 8px ${color}88)`,
+            }}
+          />
+
+          <text  x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize={size * 0.25} fill={color} > {value} </text>
         </svg>
+
+        <span
+          className="block my-4 text-[10px] text-center uppercase tracking-widest font-bold"
+          style={{ color: color }}>
+          {label}
+        </span>
       </div>
     </div>
   );
