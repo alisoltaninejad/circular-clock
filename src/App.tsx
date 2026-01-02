16b5a2827ca5257a94b85eff8a29ccba7359c247
Ali Soltani <alisolinejad@gmail.com>
2026-01-02 21:38:27 +0330

    chore: initialize Vite + Tailwind + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl pl-4 text-amber-600  drop-shadow-[0_0_4px_#ee6612]">
+        circular-clock
+      </h1>
+      <hr
+        className="divider-neon "
+      />
+      <h5 className=" text-2xl pl-4 text-emerald-400 ">
+        A stunning real-time neon clock with animated spinning border. Built
+        with <span className="text-red-400  glass text-lg">React</span> , 
+        <span className="text-red-400 glass text-lg">TypeScript </span> & 
+        <span className="text-red-400 glass text-lg">Tailwind CSS</span>  ; 
+      </h5>
+      <hr className="divider-neon"/>
+    </>
+  );
+}
+
+export default App;
